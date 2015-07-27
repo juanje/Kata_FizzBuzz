fe88abd0d79a4cff90bbb7a669319900f88be62e
Juanje Ojeda <juanje.ojeda@gmail.com>
2015-07-27 03:59:31 +0100

    Remove the TODO list

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,28 +1,3 @@
-# TODO: For a given number, return the sequence from 1 to that number but with the following sustitutions:
-#
-# * If the number is **not divisible** by **2** or **3**, then the number will **not** be changed.
-# * If the number is divisible by **2**, then sustitute the number by the word "**Fizz**".
-# * If the number is divisible by **3**, then sustitute the number by the word "**Buzz**".
-# * If the number is divisible by **2** and by **3**, then sustitute teh numbre by the word "**FizzBuzz**".
-#
-# Examples:
-#
-# Example 1: If the passed number is '2', the output should be:
-#
-#  1
-#  Fizz
-#
-# Example 2: If the passed number is '7', the output should be:
-#
-#  1
-#  Fizz
-#  Buzz
-#  Fizz
-#  5
-#  FizzBuzz
-#  7
-#
-
 def parse(number)
   if number % 2 == 0 && number % 3 == 0
     'FizzBuzz'
